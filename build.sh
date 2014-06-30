813a89ac6fdc9daaf7e864c07c9a277d000a58a9
Pablo Lezaeta <prflr88@gmail.com>
2014-06-30 02:10:27 -0400

    uo
    Signed-off-by: Pablo Lezaeta <prflr88@gmail.com>

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 for i in ./*
 do
 	cd "$i"
-	chown -R ct:users ./*
-	chmod -R 1777 ./*
+	#chown -R ct:users ./*
+	#chmod -R 1777 ./*
 	makepkg -sc -d --nobuild
 	cd $HOME/Proyectos/Jristz/PKGBUILD
 	
