2e01b8ccf9ad883e622c122ce74c13443a4867dc
Pablo Lezaeta <prflr88@gmail.com>
2014-10-04 01:43:24 -0300

    updaate script
    Signed-off-by: Pablo Lezaeta <prflr88@gmail.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 for i in $HOME/Proyectos/Jristz/PKGBUILD/*
 do
 	cd "$i"
-	#chown -R ct:users ./*
-	#chmod -R 1777 ./*
 	makepkg -sc -d --nobuild -e --verifysource
 	cd $HOME/Proyectos/Jristz/PKGBUILD
 	
