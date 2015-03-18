95c01c81adb1d54b049bce092095c9391b4519b3
Pablo Lezaeta Reyes <prflr88@gmail.com>
2015-03-18 04:39:09 -0300

    update build.sh
    Signed-off-by: Pablo Lezaeta Reyes <prflr88@gmail.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-for i in $HOME/Proyectos/Jristz/PKGBUILD/*
+for i in $HOME/Proyectos/Jristz/pkgbuild/*
 do
 	cd "$i"
 	makepkg -sc -d --nobuild -e --verifysource
-	cd $HOME/Proyectos/Jristz/PKGBUILD
+	cd $HOME/Proyectos/Jristz/pkgbuild
 	
 done
