4e2d0411c6915551f604007513696f359b79780d
Pablo Lezaeta Reyes <prflr88@gmail.com>
2025-09-16 18:48:55 -0300

    Updp

--- build.sh
+++ build.sh
@@ -1,11 +1,49 @@
 #!/usr/bin/bash
 
-for i in "$HOME/Público/PKGBUILD/"*
-do
+cd "$HOME/Público/PKGBUILD/" || exit 1
 
-	git rm "$i/"*
-	git submodule add ssh://aur@aur.archlinux.org/"$i".git "$i"
+for i in *; do
+    if [ ! -d "$i" ] && [ ! -L "$i" ]; then
+        continue
+    fi
 
-	cd "$HOME/Público/PKGBUILD/"
-	
+    if [ -L "$i" ]; then
+        if [ ! -e "$i" ]; then
+            rm "$i"
+        else
+            continue
+        fi
+    fi
+
+    need_to_rm_local=false
+    if [ -d "$i" ]; then
+        if git submodule status "$i" >/dev/null 2>&1; then
+            git submodule update --init --remote "$i"
+            git add "$i"
+            continue
+        else
+            need_to_rm_local=true
+            git rm -r --cached "$i" >/dev/null 2>&1
+        fi
+    fi
+
+    url="ssh://aur@aur.archlinux.org/${i}.git"
+    temp=$(mktemp -d)
+    if git clone --quiet "$url" "$temp"; then
+        if git -C "$temp" rev-parse HEAD >/dev/null 2>&1 && [ -n "$(git -C "$temp" ls-files)" ]; then
+            if $need_to_rm_local; then
+                rm -rf "$i"
+            fi
+            mv "$temp" "$i"
+            git submodule add "$url" "$i"
+            git add "$i"
+        else
+            rm -rf "$temp"
+            if $need_to_rm_local; then
+                rm -rf "$i"
+            fi
+        fi
+    else
+        rm -rf "$temp"
+    fi
 done
